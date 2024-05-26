b56fcce9f54fea4e41df9a40556d5d6254c45131
유재건 <127078533+orisay@users.noreply.github.com>
2024-05-26 22:55:51 +0900

    feature#dto-valid 1.inputDto valid

--- src/main/java/com/tera/pretest/context/cpumonitoring/dto/input/GetCpuUsageRateByMinute.java
+++ src/main/java/com/tera/pretest/context/cpumonitoring/dto/input/GetCpuUsageRateByMinute.java
@@ -2,6 +2,7 @@ package com.tera.pretest.context.cpumonitoring.dto.input;
 
 import lombok.*;
 
+import javax.validation.constraints.NotNull;
 import java.sql.Timestamp;
 
 @ToString
@@ -11,5 +12,7 @@ import java.sql.Timestamp;
 @NoArgsConstructor
 @AllArgsConstructor
 public class GetCpuUsageRateByMinute {
+
+    @NotNull(message = "조회를 원하는 날과 시간이 입력되지 않았습니다.")
     private Timestamp startDay;
 }
